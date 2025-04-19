d9f11cfb19d114e64ec8df59c204dcd53ea9490a
Serhii Lebedynskyi <starf7455@gmail.com>
2025-04-19 09:25:25 +0300

    made a program that prints the entered number apeak

---
+++ MySecondHW.py
@@ -0,0 +1,3 @@
+user_name = input("Введіть ваше ім'я:")
+user_numeric = input(f"{user_name}, введіть 4-х значне число:")
+print (f" {user_numeric[0]} \n {user_numeric[1]} \n {user_numeric[2]} \n {user_numeric[3]} \n")
