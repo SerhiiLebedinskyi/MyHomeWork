3ad513ba15fb661e33cce7ed4d72705746b074b2
Serhii Lebedynskyi <starf7455@gmail.com>
2025-04-27 19:16:21 +0300

    wrote a simple calculator

---
+++ calculator.py
@@ -0,0 +1,20 @@
+first_num = int(input("Введіть перше число:").strip())
+second_num = int(input("Введіть друге число:").strip())
+action_on_numbers = input("Вкажіть дію (+,-,*,/):").strip()
+
+if action_on_numbers == "/" and second_num == 0:
+    result = "ДІЛЕННЯ НА 0 НЕМОЖЛИВЕ"
+elif action_on_numbers == "/":
+    result = first_num / second_num
+elif action_on_numbers == "*":
+    result = first_num * second_num
+elif action_on_numbers == "+":
+    result = first_num + second_num
+elif action_on_numbers == "-":
+    result = first_num - second_num
+else:
+    result = "ВВЕДЕНО ДІЮ НАД ЧИСЛАМИ, ЩО НЕ ВХОДИТЬ ДО (+,-,*,/)"
+
+print(
+    f"Перше число: {first_num}\nДія над числами: {action_on_numbers}\nДруге число: {second_num}\nРезультат = {result}"
+)
