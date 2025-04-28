d1dd8d2236c30042b6dfa11db482f916b8ac682f
Serhii Lebedynskyi <starf7455@gmail.com>
2025-04-28 23:47:56 +0300

    I cut the list in two

---
+++ slice_list.py
@@ -0,0 +1,15 @@
+initial_list = [1, 2, 3, 4, 5]
+length_initial = len(initial_list)
+if length_initial <= 1:
+    first_list = second_list = initial_list
+else:
+    half_length_initial = length_initial // 2
+    if length_initial % 2 == 0:
+        first_list = initial_list[:half_length_initial]
+        second_list = initial_list[half_length_initial:]
+    elif length_initial % 2 != 0:
+        first_list = initial_list[: half_length_initial + 1]
+        second_list = initial_list[half_length_initial + 1 :]
+
+output_list = [first_list, second_list]
+pass
