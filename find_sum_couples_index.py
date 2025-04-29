fba4ee668fe5db2a9576bb928a0effb925a74de6
Serhii Lebedynskyi <starf7455@gmail.com>
2025-04-29 20:49:49 +0300

    I add up the values of even index. I multiply the sum by the last value.

---
+++ find_sum_couples_index.py
@@ -0,0 +1,5 @@
+initial_list = [6]
+sum_values_paired_index = sum(initial_list[::2])
+finish_value = initial_list[-1] if initial_list else 0
+final_settlement = sum_values_paired_index * finish_value
+pass
